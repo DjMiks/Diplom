fd10af22aa285ced1a24b84bcade595bc207a7d8
DjMiks <9669374@gmail.com>
2023-05-23 15:48:11 +0300

    01.APP Creation

---
+++ Diplom/Diplom/DiplomApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiplomApp.swift
+//  Diplom
+//
+//  Created by Максим Ялынычев on 04.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct DiplomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
